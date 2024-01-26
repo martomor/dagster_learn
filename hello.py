38ebcdfe1e33bd77a317b98da723d009dbf687fc
martin0ecovco <martin.moreno@ecovis.co>
2024-01-26 11:53:19 -0500

    adding the first op

---
+++ hello.py
@@ -0,0 +1,11 @@
+import os
+from dagster import job, op, get_dagster_logger
+
+
+@op
+def get_file_sizes():
+    files = [f for f in os.listdir(".") if os.path.isfile(f)]
+    for f in files:
+        get_dagster_logger.info(f"Size of {f} is {os.path.getsize(f)}")
+
+
